d68ba992cba408a43b907ff1676a1ddace0e3d53
Ronald Dilley <rdilley@usgs.gov>
2015-11-10 12:58:51 -0600

    Updated

--- run-development-shell.sh
+++ run-development-shell.sh
@@ -1,18 +1,21 @@
 # The image/container to run against
 if [ $1 ]; then
     image=$1
+    the_gid=''
+    the_uid=''
 else
     image="usgs-espa/ubuntu-development"
+
+    # These solve file creation problems for developers
+    # The group ID of the person kicking off the container
+    the_gid=`id -g`
+    # The group ID of the person kicking off the container
+    the_uid=`id -u`
 fi
 
-# These solve file creation problems for developers
-# The group ID of the person kicking off the container
-the_gid=`id -g`
-# The group ID of the person kicking off the container
-the_uid=`id -u`
-
 # Create the head of the command
-cmd="docker run -i"
+cmd_head="docker run -i"
+cmd_tail="-t ${image} ${the_gid} ${the_uid}"
 
 # If this file exists, then use it for mounting source code volumes that will
 # override the defaulted versions
@@ -24,7 +27,7 @@ fi
 
 # Create the tail of the command, specifying the image and the user and group
 # IDs to run as in the container for file creation problems
-cmd=${cmd}" -t ${image} ${the_gid} ${the_uid}"
+cmd=${cmd_head}" "${cmd_tail}
 
 # Let the user know what we are doing, and then execute the command
 echo $cmd
