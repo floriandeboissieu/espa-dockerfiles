15fe7c6cfebfa3a7b21567512a4a6b4434770682
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2016-07-29 09:08:25 -0500

    Adding an example command line for executing an espa-processing container

---
+++ example-run-cmd.sh
@@ -0,0 +1,32 @@
+docker run --rm --tty \
+  --cap-add LINUX_IMMUTABLE \
+  --hostname $USER-cli \
+  --name $USER-cli \
+  --volume <HOST>/work-dir:/home/espa/work-dir:rw \
+  --volume <HOST>/output-data:/home/espa/output-data:rw \
+  --volume <HOST>/auxiliaries:/usr/local/auxiliaries:ro \
+  --volume <HOST>/input-data:/home/espa/input-data:ro \
+  --volume $HOME/.usgs:/home/espa/.usgs:ro \
+  --workdir /home/espa \
+  espa.centos.science:0.2.0 `id -g` `id -u` \
+  cli.py \
+    --order-id frog \
+    --input-product-id LE70390372003148EDC00 \
+    --product-type landsat \
+    --output-format envi \
+    --input-url file:///home/espa/input-data/LE7/LE70390372003148EDC00.tar.gz \
+    --include-top-of-atmosphere \
+    --include-brightness-temperature \
+    --include-sr-ndvi \
+    --include-cfmask \
+    --include-surface-water-extent \
+    --include-statistics \
+    --pixel-size 60 \
+    --extent-minx 557300.0 \
+    --extent-maxx 642200.0 \
+    --extent-miny 3596000.0 \
+    --extent-maxy 3708200.0 \
+    --resample-method cubic \
+    --target-projection utm \
+    --utm-north-south north \
+    --utm-zone 10
