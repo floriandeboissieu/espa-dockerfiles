b27ac67b4698ff5c6f3712beb5232406b625f7fd
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2017-03-01 16:18:17 -0600

    updated versions

--- fix-espa-versions.sh
+++ fix-espa-versions.sh
@@ -6,9 +6,9 @@ cat ${2} | sed \
     -e "s/ESPA_L2QA_TOOLS/l2qa_tools_v1.2.0/" \
     -e "s/ESPA_CLOUD_MASKING/cfmask-v2.0.2/" \
     -e "s/ESPA_SURFACE_REFLECTANCE/dev_lasrc3.5.2/" \
-    -e "s/ESPA_LAND_SURFACE_TEMPERATURE/dev_0.1.2/" \
+    -e "s/ESPA_LAND_SURFACE_TEMPERATURE/dev_0.1.3/" \
     -e "s/ESPA_SPECTRAL_INDICES/spectral_indices_v2.5.0/" \
-    -e "s/ESPA_SURFACE_WATER_EXTENT/espa-v2.15.0/" \
+    -e "s/ESPA_SURFACE_WATER_EXTENT/fb-dswe-1.1.3/" \
     -e "s/ESPA_ELEVATION/v2.2.0/" \
     -e "s/ESPA_PYTHON_LIBRARY/v1.0.1/" \
-    -e "s/ESPA_PROCESSING/rb-2.16.0/" > ${3}
+    -e "s/ESPA_PROCESSING/rb-2.17.0/" > ${3}
