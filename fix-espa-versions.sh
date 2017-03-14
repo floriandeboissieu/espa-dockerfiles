b0e6d09c1281fdaaa8a85817349f28a9356b3919
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2017-03-14 10:00:28 -0500

    Updated to correct branches and tags

--- fix-espa-versions.sh
+++ fix-espa-versions.sh
@@ -3,9 +3,9 @@
 cat ${2} | sed \
     -e "s/ESPA_VERSION/${1}/" \
     -e "s/ESPA_PRODUCT_FORMATTER/product_formatter_v1.11.0/" \
-    -e "s/ESPA_L2QA_TOOLS/l2qa_tools_v1.2.0/" \
+    -e "s/ESPA_L2QA_TOOLS/dev_v1.3.0/" \
     -e "s/ESPA_CLOUD_MASKING/cfmask-v2.0.2/" \
-    -e "s/ESPA_SURFACE_REFLECTANCE/dev_lasrc3.5.2/" \
+    -e "s/ESPA_SURFACE_REFLECTANCE/surface_reflectance_lasrc3.5.2/" \
     -e "s/ESPA_LAND_SURFACE_TEMPERATURE/dev_0.1.3/" \
     -e "s/ESPA_SPECTRAL_INDICES/spectral_indices_v2.5.0/" \
     -e "s/ESPA_SURFACE_WATER_EXTENT/fb-dswe-1.1.3/" \
