58e8c373ed3bfad4415f4131c8fc299903766dee
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2017-04-04 09:37:16 -0500

    updated to current tags and branches

--- fix-espa-versions.sh
+++ fix-espa-versions.sh
@@ -2,13 +2,13 @@
 
 cat ${2} | sed \
     -e "s/ESPA_VERSION/${1}/" \
-    -e "s/ESPA_PRODUCT_FORMATTER/dev_v1.11.1/" \
-    -e "s/ESPA_L2QA_TOOLS/dev_v1.3.0/" \
+    -e "s/ESPA_PRODUCT_FORMATTER/product_formatter_v1.11.1/" \
+    -e "s/ESPA_L2QA_TOOLS/l2qa_tools_v1.3.0/" \
     -e "s/ESPA_CLOUD_MASKING/cfmask-v2.0.2/" \
-    -e "s/ESPA_SURFACE_REFLECTANCE/dev_march2017/" \
-    -e "s/ESPA_LAND_SURFACE_TEMPERATURE/dev_0.1.3/" \
+    -e "s/ESPA_SURFACE_REFLECTANCE/surface_reflectance_mar2017/" \
+    -e "s/ESPA_LAND_SURFACE_TEMPERATURE/lst-rit-v0.1.3/" \
     -e "s/ESPA_SPECTRAL_INDICES/spectral_indices_v2.5.0/" \
-    -e "s/ESPA_SURFACE_WATER_EXTENT/fb-dswe-1.1.3/" \
-    -e "s/ESPA_ELEVATION/v2.2.0/" \
-    -e "s/ESPA_PYTHON_LIBRARY/v1.0.1/" \
-    -e "s/ESPA_PROCESSING/rb-2.17.0/" > ${3}
+    -e "s/ESPA_SURFACE_WATER_EXTENT/espa-v2.17.0/" \
+    -e "s/ESPA_ELEVATION/rb-v2.3.0/" \
+    -e "s/ESPA_PYTHON_LIBRARY/rb-v1.1.0/" \
+    -e "s/ESPA_PROCESSING/rb-2.18.0/" > ${3}
