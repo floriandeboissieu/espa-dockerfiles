c64b7dd70686431cb0a041bb44b61b8cc4fbd532
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-08-10 16:45:16 -0500

    BLD: update project versions

--- fix-espa-versions.sh
+++ fix-espa-versions.sh
@@ -2,13 +2,13 @@
 
 cat ${2} | sed \
     -e "s/ESPA_VERSION/${1}/" \
-    -e "s/ESPA_PRODUCT_FORMATTER/product_formatter_v1.13.0/" \
+    -e "s/ESPA_PRODUCT_FORMATTER/product_formatter_v1.13.1/" \
     -e "s/ESPA_L2QA_TOOLS/l2qa_tools_v1.5.0/" \
-    -e "s/ESPA_SURFACE_REFLECTANCE/surface_reflectance_june2017/" \
-    -e "s/ESPA_LAND_SURFACE_TEMPERATURE/lst-rit-v0.3.0/" \
+    -e "s/ESPA_SURFACE_REFLECTANCE/surface_reflectance_aug2017/" \
+    -e "s/ESPA_LAND_SURFACE_TEMPERATURE/lst-rit-v0.3.1/" \
     -e "s/ESPA_SPECTRAL_INDICES/spectral_indices_v2.6.0/" \
-    -e "s/ESPA_SURFACE_WATER_EXTENT/espa-v2.17.0/" \
+    -e "s/ESPA_SURFACE_WATER_EXTENT/dswe-v2.0/" \
     -e "s/ESPA_ELEVATION/v2.3.0/" \
     -e "s/ESPA_REPROJECTION/v1.0.1/" \
     -e "s/ESPA_PYTHON_LIBRARY/v1.1.0/" \
-    -e "s/ESPA_PROCESSING/rb-2.20.0/" > ${3}
+    -e "s/ESPA_PROCESSING/v2.22.0/" > ${3}
